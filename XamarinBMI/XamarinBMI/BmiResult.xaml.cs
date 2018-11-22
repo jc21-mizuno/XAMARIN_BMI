c453a4b764925e14acf610988ccc650d34778916
瞳 水野 <160801@jc-21.jp>
2018-11-22 12:28:44 +0900

    BMI課題できた

---
+++ XamarinBMI/XamarinBMI/BmiResult.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinBMI
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BmiResult : ContentPage
+	{
+        private float height;
+        private int weight;
+
+        public BmiResult ()
+		{
+			InitializeComponent ();
+            buttonBack.Clicked += buttonBackClicked;
+        }
+
+        public BmiResult(float height, int weight)
+        {
+
+            InitializeComponent();
+            this.height = height;
+            this.weight = weight;
+            labelBMI.Text = "BMI: " + weight / height / height;
+            buttonBack.Clicked += buttonBackClicked;
+        }
+        private void buttonBackClicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
--- XamarinBMI/XamarinBMI/MainPage.xaml.cs
+++ XamarinBMI/XamarinBMI/MainPage.xaml.cs
@@ -11,7 +11,52 @@ namespace XamarinBMI
     {
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            ButtonBMI.Clicked += ButtonBMIClickEvent;
+            ButtonBMI2.Clicked += ButtonBMIClickEvent2;
         }
+
+        private void ButtonBMIClickEvent(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //throw new NotImplementedException();
+                float height = float.Parse(EntryHeight.Text);
+                int weight = int.Parse(EntryWeight.Text);
+
+                if (height > 2.72)
+                {
+                    height = height / 100;
+                }
+
+                LabelResult.Text = "BMI: " + weight / height / height;
+
+            }
+            catch
+            {
+                LabelResult.Text = "数値を正しく入力してください";
+            }
+        }
+        
+        private void ButtonBMIClickEvent2(object sender, EventArgs e)
+        {
+            try
+            {
+                float height = float.Parse(EntryHeight.Text);
+                int weight = int.Parse(EntryWeight.Text);
+
+                if (height > 2.72)
+                {
+                    height = height / 100;
+                }
+                Navigation.PushModalAsync(new NavigationPage(new BmiResult(height, weight)));
+            }
+            catch
+            {
+                LabelResult.Text = "数値を正しく入力してください";
+            }
+        }
+        
     }
 }
